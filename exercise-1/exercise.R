d566441524f890ca16d67b35400273fedc784496
Robin-Yang <yangy87@uw.edu>
2018-04-05 21:57:30 -0700

    finish exercises

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -24,4 +24,4 @@ too_expensive <- total_cost > 1000
 
 # Create a variable `max_puppies`, which is the number of puppies you can 
 # afford for $1,000
-max_puppies <- floor(1000/puppy_price)
+max_puppies <- floor(1000 / puppy_price)
